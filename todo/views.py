dbdd10d70d7f37bcf70d212f61a0a95822b104a0
Muratbek Jusupov <muratbekjusupov@Muratbeks-MacBook-Air.local>
2024-08-29 20:11:43 +0800

    Templates + Views + Urls used

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
 # Create your views here.
+from .models import*
+
+class HomePageView(ListView):
+    model = List
+    template_name = 'index.html'
+    context_object_name = 'list_types'
