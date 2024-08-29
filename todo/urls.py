a54aa9bee02a4aafd492a904845f8a5fd7833993
Muratbek Jusupov <muratbekjusupov@Muratbeks-MacBook-Air.local>
2024-08-29 20:47:41 +0800

    ListView for List and Task Model + Templates

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import*
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home')
+    path('', HomePageView.as_view(), name='home'),
+    path('todo-list/<int:list_id>/', TodoListView.as_view(), name='todolist'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -7,3 +7,12 @@ class HomePageView(ListView):
     model = List
     template_name = 'index.html'
     context_object_name = 'list_types'
+
+class TodoListView(ListView):
+    model = Task
+    template_name = 'todo_list.html'
+    context_object_name = 'task_list'
+
+    def get_queryset(self):
+        list_obj = List.objects.get(id=self.kwargs['list_id'])
+        return Task.objects.filter(list=list_obj)
